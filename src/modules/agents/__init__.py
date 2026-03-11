d420cffe9def9b4b3907539d6deea885a42a2652
Jacob <jheglun2@gmail.com>
2026-03-11 15:08:26 -0500

    added MAIC

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -1,9 +1,11 @@
 from .rnn_agent import RNNAgent
 from .rnn_ns_agent import RNNNSAgent
 from .rnn_feature_agent import RNNFeatureAgent
+from .maic_agent import MAICAgent
 
 
 REGISTRY = {}
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["rnn_ns"] = RNNNSAgent
 REGISTRY["rnn_feat"] = RNNFeatureAgent
+REGISTRY['maic'] = MAICAgent
