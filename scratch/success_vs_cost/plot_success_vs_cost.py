f5974e4ba9dbd5cccefa883f3d798c52f5fe677c
Jaye Heglund <jheglun2@illinois.edu>
2026-07-20 15:15:48 -0500

    updated plots

--- scratch/success_vs_cost/plot_success_vs_cost.py
+++ scratch/success_vs_cost/plot_success_vs_cost.py
@@ -1,8 +1,19 @@
 import os
+from os.path import join
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pandas as pd
+
+font_path = join("data", "times.ttf")
+import matplotlib.font_manager as fm
+
+fm.fontManager.addfont(font_path)
+
+# Activate the font
+plt.rcParams["font.family"] = "Times New Roman"
+
+# mpl.rc("font", family="Times")
 
 
 def create_success_vs_cost_plot() -> None:
@@ -43,16 +54,39 @@ def create_success_vs_cost_plot() -> None:
         )
 
     ax.legend()
-    ax.set_xlabel("Mission Success Prob., $p_c$", fontsize=12)
-    ax.set_ylabel("Communication Cost", fontsize=12)
+    ax.set_xlabel("Task Satis. Rate", fontsize=13)
+    # ax.set_xlabel("Mission Success Prob., $p_c$", fontsize=12)
+    ax.set_ylabel("Network Cost", fontsize=13)
     ax.yaxis.set_major_locator(ticker.MaxNLocator(4))
     ax.grid()
 
     plt.tight_layout()
-    save_path = os.path.join(load_dir, "success_vs_cost.pdf")
-    plt.savefig(save_path)
+
+    # place the arrow
+    text_x = 0.95
+    plt.annotate(
+        "",
+        xy=(0.975, 0.52),  # (x, y) coordinates where the arrow points
+        xytext=(0.975, 0.73),  # (x, y) coordinates where the text is placed
+        arrowprops=dict(color="red"),  # Makes the arrow red
+    )
+    plt.text(x=text_x, y=0.68, s="Lower\ncost", color="red")
+
+    # place the label separately
+    # plt.annotate(
+    #     "Lower cost",  # The text label
+    #     color="red",
+    #     xy=(0.98, 0.5),  # (x, y) coordinates where the arrow points
+    #     xytext=(0.98, 0.75),  # (x, y) coordinates where the text is placed
+    #     arrowprops=dict(facecolor="red"),  # Makes the arrow red
+    # )
+
+    # save_path = os.path.join(load_dir, "success_vs_cost.pdf")
+    # plt.savefig(save_path)
     save_path = os.path.join(load_dir, "success_vs_cost.png")
+    # plt.show()
     plt.savefig(save_path, dpi=500)
+    plt.close()
 
 
 create_success_vs_cost_plot()
